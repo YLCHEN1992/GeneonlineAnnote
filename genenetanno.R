4c0eaedacf6df50f7ad88ff623e1c55759b51054
YingLian Chen <78669829+YLCHEN1992@users.noreply.github.com>
2023-11-07 17:14:04 +0800

    Add files via upload

--- genenetanno.R
+++ genenetanno.R
@@ -4,20 +4,22 @@ parameter2 <- args[2]
 id=read.csv(parameter1,sep="\t")
 x=read.csv(parameter2)
 genes=unique(unlist(strsplit(x$Genes,";")))
-geneids=id$GeneID[id$Symbol%in%genes]
+geneids=unique(id$GeneID[id$Symbol%in%genes])
+pp=id[which(id$GeneID%in%geneids),]
+pp=pp[match(geneids,pp$GeneID),]
 if(file.exists("./netcat")==TRUE){cat("netcat Existed!\n")}else{
 dir.create("./netcat",recursive=TRUE);cat("netcat Created\n")}
-for(i in 1:length(geneids)){cat(paste("Running","i",length(geneids)));system(paste("netcat.bat",geneids[i]))}
+for(i in 1:length(geneids)){cat(paste("Running",i,geneids[i],as.character(pp[i,3]),"Total num:",length(geneids),"\n"));system(paste("netcat.bat",geneids[i]))}
 z=readLines("geneinfo")
 z=gsub("<dd>","",z)
 z=gsub("</dd>","",z)
+z=gsub("        ","",z)
 z=gsub("<a href=\"#gene-expression\">See more</a>","",z)
 pss=grep("df~",z)
-zs=paste(z[1:(pss[1]-1)],collapse=";")
-for(i in 1:(length(pss)-1)){zs=c(zs,paste(z[(pss[i]+1):(pss[i+1]-1)],collapse=";"))}
+zs=paste(z[2:(pss[1]-1)],collapse=";")
+if (length(pss)>1) {for(i in 1:(length(pss)-1)){zs=c(zs,paste(z[(pss[i]+2):(pss[i+1]-1)],collapse=";"))}}
 zframe=data.frame(geneid=geneids,summary=zs)
-pp=id[which(id$GeneID%in%zframe$geneid),]
-pp=pp[match(zframe$geneid,pp$GeneID),]
 zframe=cbind(pp[,3],zframe,pp[,c(5,7:10)])
-write.csv(zframe,"genesummary.csv",row.names=F)
+colnames(zframe)[1]="NCBIGene"
+write.csv(zframe,paste(gsub(".csv","",as.character(parameter2)),"genesummary.csv",sep="_"),row.names=F)
 file.remove("geneinfo")
--- netcat.bat
+++ netcat.bat
@@ -1,7 +1,7 @@
 @echo off
 %1
 set GID=%1
-if not exist ./netcat/G%GID% (curl -s https://www.ncbi.nlm.nih.gov/gene/%GID% > ./netcat/G%GID%)
+if not exist ./netcat/G%GID% (curl https://www.ncbi.nlm.nih.gov/gene/%GID% -o ./netcat/G%GID% --ssl-no-revoke)
 echo %GID% >>geneinfo
 findstr "]</dd>" .\netcat\G%GID% >>geneinfo
 findstr "href=\"#gene-expression\">See" .\netcat\G%GID% >>geneinfo
